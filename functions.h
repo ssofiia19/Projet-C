fb2bfc2dd95452be9861bc9ae76e4bcbcf5ac747
ssofiia19 <150902163+ssofiia19@users.noreply.github.com>
2024-03-26 14:20:02 +0000

    functions_header

---
+++ functions.h
@@ -0,0 +1,19 @@
+
+
+int regions();
+
+int display();
+
+int factorielle(int n);
+
+int somme(int n);
+
+void affich (int x);
+
+int equal(int a, int b);
+
+void affichage (int x);
+
+int age(int x);
+
+void show_region();
